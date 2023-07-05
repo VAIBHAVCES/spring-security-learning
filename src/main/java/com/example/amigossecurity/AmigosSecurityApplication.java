a041a74ab23720b799732d2dd8b90a3d1f91be53
Vaibhav Jain <jainva@vmware.com>
2023-07-05 23:54:03 +0530

    First commit regarding spring security

---
+++ src/main/java/com/example/amigossecurity/AmigosSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.example.amigossecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Res
+public class AmigosSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosSecurityApplication.class, args);
+	}
+
+}
