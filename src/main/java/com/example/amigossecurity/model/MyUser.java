d9f1e30d9da402f91835968f17f020483fa1904c
Vaibhav Jain <jainva@vmware.com>
2023-07-07 23:54:16 +0530

    Learning Spring Security: Adding user models

---
+++ src/main/java/com/example/amigossecurity/model/MyUser.java
@@ -0,0 +1,61 @@
+package com.example.amigossecurity.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+//Model for storing it in security
+public class MyUser implements UserDetails {
+
+    String name ;
+    String email;
+    String phone;
+    String username;
+    String password;
+
+
+
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+//         this should be true for authentication to succeed
+//         but you can change logic in future
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true ;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
---
+++ src/main/java/com/example/amigossecurity/model/Role.java
@@ -0,0 +1,7 @@
+package com.example.amigossecurity.model;
+
+public enum Role {
+    USER,
+    ADMIN
+
+}
